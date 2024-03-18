2b3bd6c8af9a270d97a716e30cf0b7ef98c7016c
FilipAkbyr <101974874+FilipAkbyr@users.noreply.github.com>
2024-03-18 10:45:44 +0100

    Confirmation

--- pages/adverts.tsx
+++ pages/adverts.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, Grid, Paper, Skeleton, Typography } from '@mui/material';
+import { Box, Button, Grid, Modal, Paper, Skeleton, Typography, colors } from '@mui/material';
 import { House, useHousesQueryQuery, useUserDataQuery } from '../generated/graphql';
 import Navbar from '../components/navbar';
 import Link from 'next/link';
@@ -56,6 +56,7 @@ console.log(userData);
       await deleteHouseMutation({ variables: { propertyId: propertyId } });
       setProperties(properties.filter(property => property.id !== propertyId));
       toast.success('Property deleted successfully', {containerId: "advertsToastId", position: "top-right", autoClose: 2000});
+      handleClose();
     } catch (error) {
       toast.error('Property could not be deleted', {containerId: "advertsToastId", position: "top-right", autoClose: 2000});
     }
@@ -71,10 +72,46 @@ console.log(userData);
 
   console.log(properties);
 
+  const [deletePropertyId, setDeletePropertyId] = useState(null as string | null | undefined);
+  const [open, setOpen] = useState(false);
+  const handleOpen = (propertyId: string | null | undefined) => {setOpen(true); setDeletePropertyId(propertyId);};
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+  
   return (
     <>
       <Navbar></Navbar>
       <Container>
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              You are about to delete a property
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Are you sure you want to delete this property?
+            </Typography>
+            <Box sx={{ mt: 2 }}>
+              <Button onClick={() => handleDeleteClick(deletePropertyId)} variant="contained" color="error">
+                Delete
+              </Button>
+              <Button onClick={handleClose} variant="contained" color="success" sx={{ marginLeft: 2 }}>
+                Cancel
+              </Button>
+            </Box>
+          </Box>
+        </Modal>
         <Box>
         <ToastContainer containerId={"advertsToastId"}/>
         </Box>
@@ -145,7 +182,7 @@ console.log(userData);
                         filter: "brightness(1.2)",
                       },
                     }}
-                    onClick={() => handleDeleteClick(property.id)}
+                    onClick={() => handleOpen(property.id)}
                   />
                 ) : null}
                   </Paper>
